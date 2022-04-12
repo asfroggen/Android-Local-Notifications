09107b246a0c64cac3bb1ad4b4490cf5177be299
Esau <yosoypawn@hotmail.com>
2022-04-12 17:11:38 -0500

    checkpoint 4

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -32,6 +32,7 @@ private val REQUEST_CODE = 0
 private val FLAGS = 0
 
 // TODO: Step 1.1 extension function to send messages (GIVEN)
+
 /**
  * Builds and delivers the notification.
  *
@@ -41,8 +42,14 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     // Create the content intent for the notification, which launches
     // this activity
     // TODO: Step 1.11 create intent
-
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
     // TODO: Step 1.12 create PendingIntent
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
 
     // TODO: Step 2.0 add style
 
@@ -50,12 +57,22 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
 
     // TODO: Step 1.2 get an instance of NotificationCompat.Builder
     // Build the notification
+    val builder = NotificationCompat.Builder(
+        applicationContext,
+        applicationContext.getString(R.string.egg_notification_channel_id)
+    ).setSmallIcon(R.drawable.cooked_egg)
+        .setContentTitle(applicationContext.getString(R.string.notification_title))
+        .setContentText(messageBody)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
+
+    // TODO: Step 1.13 set content intent
+
+    notify(NOTIFICATION_ID, builder.build())
 
     // TODO: Step 1.8 use the new 'breakfast' notification channel
 
     // TODO: Step 1.3 set title, text and icon to builder
-
-    // TODO: Step 1.13 set content intent
 
         // TODO: Step 2.1 add style to builder
 
@@ -68,3 +85,10 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
 }
 
 // TODO: Step 1.14 Cancel all notifications
+/**
+ * Cancels all notifications.
+ *
+ */
+fun NotificationManager.cancelNotifications() {
+    cancelAll()
+}
